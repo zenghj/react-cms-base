d2547b13cb400a8baf84e0684932889ed3daf90e
zenghj <zenghj@outlook.com>
2018-11-03 16:31:00 +0800

    make store exportable

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Route , Router, Switch} from 'react-router-dom';
 
 import PrivateRoute from './components/PrivateRoute'
-import configureStore from './store';
+import configureStore from './store/configureStore';
 import './index.css';
 import App from './App';
 import SignIn from './pages/SignIn';
 import * as serviceWorker from './serviceWorker';
 
-const store = configureStore();
+export const store = configureStore();
 
 const render = Component => {
   return ReactDOM.render(
