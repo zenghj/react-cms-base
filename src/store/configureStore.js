3fb8be2214e6429fbe7c26cf1ebf2f708c6c771b
zenghj <zenghj@outlook.com>
2018-11-06 17:22:07 +0800

    firefox not support

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,11 +3,14 @@ import thunk from 'redux-thunk';
 
 import rootReducer from './reducers'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // HMR redux store 
 // https://github.com/facebook/create-react-app/issues/2317
 const configureStore = () => {
-  const store = createStore(rootReducer, 
-    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+  
+  const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+  ))
   
   if (process.env.NODE_ENV !== 'production') {
     if(module.hot) {
